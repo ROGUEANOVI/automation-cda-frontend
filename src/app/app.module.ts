e7af4afb0b477417a6ca72849002a4283c0e8ffb
ROGUEANOVI <ovidioromero66@gmail.com>
2022-10-26 18:17:32 -0500

    Instalacion de bootstrap 5.2.2 y creacion de componentes login y template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavBarComponent } from './components/template/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
