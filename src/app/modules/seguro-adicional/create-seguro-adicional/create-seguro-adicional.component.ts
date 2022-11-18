140b76e0537871383617adceff730c7d10f8f5a7
ROGUEANOVI <ovidioromero66@gmail.com>
2022-11-18 08:42:10 -0500

    Crud completo de repuesto

--- src/app/modules/seguro-adicional/create-seguro-adicional/create-seguro-adicional.component.ts
+++ src/app/modules/seguro-adicional/create-seguro-adicional/create-seguro-adicional.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { SeguroAdicional } from 'src/app/models/seguroadicional';
 import { SeguroAdicionalService } from 'src/app/services/seguros-adicionales/seguro-adicional.service';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-create-seguro-adicional',
